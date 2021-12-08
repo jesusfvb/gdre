b3d8cf9b4d9d099c61e7c413055b260dcf5076e0
Jesus F. Vazquez Biltre <jesusfvb2@gmail.com>
2021-12-07 23:48:21 -0500

    F: Barra de Navegación

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,8 +1,9 @@
 import {ReactElement, useState} from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Login from "./paginas/Login";
 import axios from "axios";
 import img_2 from "./img/identidad/img_2.png";
+import BarraDeNavegacion from "./componentes/BarraDeNavegacion";
 
 export default function App(): ReactElement {
     axios.defaults.baseURL = 'http://localhost:8080';
@@ -21,7 +22,7 @@ export default function App(): ReactElement {
             })
             .catch(error => console.error(error))
     }
-    const serrarSesion = () => {
+    const cerrarSesion = () => {
         localStorage.clear()
         setSesion(null)
     }
@@ -37,12 +38,12 @@ export default function App(): ReactElement {
         <BrowserRouter>
             <Routes>
                 {(sesion === null) ? <Route path="*" element={<Login iniciarSesion={iniciarSesion}/>}/> :
-                    <Route path="*" element={
-                        <div>
-                            <h1>Dentro</h1>
-                            <button onClick={serrarSesion}>salir</button>
-                        </div>
-                    }/>
+                    <Route element={<BarraDeNavegacion cerrarSesion={cerrarSesion}/>}>
+                        <Route path="/" element={<Navigate to="/principal"/>}/>
+                        <Route path="/principal" element={<div>
+                            <h1>Ojo</h1>
+                        </div>}/>
+                    </Route>
                 }
             </Routes>
         </BrowserRouter>
