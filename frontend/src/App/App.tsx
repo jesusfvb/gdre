163eeb20a141a8edef302ba57bea554a69dbd380
Jesus F. Vazquez Biltre <jesusfvb2@gmail.com>
2021-12-07 20:45:28 -0500

    F: B: Seguridad

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -2,26 +2,48 @@ import {ReactElement, useState} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./paginas/Login";
 import axios from "axios";
+import img_2 from "./img/identidad/img_2.png";
 
 export default function App(): ReactElement {
     axios.defaults.baseURL = 'http://localhost:8080';
-
-    const [autenticado, setAutenticado] = useState<string>()
+    const body = document.getElementsByTagName("body")[0]
+    const [sesion, setSesion] = useState<string | null>(localStorage.getItem("jwt"))
 
     const iniciarSesion = (usuario: string, contrasenna: string): void => {
-        const cuerpo = new FormData()
-        cuerpo.append("usuario", usuario)
-        cuerpo.append("usuario", usuario)
         axios
-            .post("/login", cuerpo)
-            .then(datos => console.log(datos))
+            .post("/login", {
+                usuario: usuario,
+                contrasenna: contrasenna
+            })
+            .then(datos => {
+                localStorage.setItem("jwt", datos.data)
+                setSesion(datos.data)
+            })
             .catch(error => console.error(error))
     }
+    const serrarSesion = () => {
+        localStorage.clear()
+        setSesion(null)
+    }
 
+    if (sesion === null) {
+        body.style.backgroundImage = `url(${img_2})`;
+        body.style.backgroundRepeat = "no-repeat";
+        body.style.backgroundSize = "cover";
+    } else {
+        body.style.background = "none";
+    }
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="*" element={<Login iniciarSesion={iniciarSesion}/>}/>
+                {(sesion === null) ? <Route path="*" element={<Login iniciarSesion={iniciarSesion}/>}/> :
+                    <Route path="*" element={
+                        <div>
+                            <h1>Dentro</h1>
+                            <button onClick={serrarSesion}>salir</button>
+                        </div>
+                    }/>
+                }
             </Routes>
         </BrowserRouter>
     );
