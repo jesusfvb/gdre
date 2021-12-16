255e72ebd218675e1e02310f17d0e5e302f3e8a4
Daniel Aguilar Torres <daxs1310@gmail.com>
2021-12-16 11:41:59 -0500

    F: Error 404

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -1,50 +1,58 @@
-import {ReactElement, useState} from "react";
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import { ReactElement, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./paginas/Login";
 import axios from "axios";
 import img_2 from "./img/identidad/img_2.png";
 import BarraDeNavegacion from "./componentes/BarraDeNavegacion";
 import Principal from "./paginas/Principal";
+import E404 from "./paginas/E404";
 
 export default function App(): ReactElement {
-    axios.defaults.baseURL = 'http://localhost:8080';
-    const body = document.getElementsByTagName("body")[0]
-    const [sesion, setSesion] = useState<string | null>(localStorage.getItem("jwt"))
+  axios.defaults.baseURL = "http://localhost:8080";
+  const body = document.getElementsByTagName("body")[0];
+  const [sesion, setSesion] = useState<string | null>(
+    localStorage.getItem("jwt")
+  );
 
-    const iniciarSesion = (usuario: string, contrasenna: string): void => {
-        axios
-            .post("/login", {
-                usuario: usuario,
-                contrasenna: contrasenna
-            })
-            .then(datos => {
-                localStorage.setItem("jwt", datos.data)
-                setSesion(datos.data)
-            })
-            .catch(error => console.error(error))
-    }
-    const cerrarSesion = () => {
-        localStorage.clear()
-        setSesion(null)
-    }
+  const iniciarSesion = (usuario: string, contrasenna: string): void => {
+    axios
+      .post("/login", {
+        usuario: usuario,
+        contrasenna: contrasenna,
+      })
+      .then((datos) => {
+        localStorage.setItem("jwt", datos.data);
+        setSesion(datos.data);
+      })
+      .catch((error) => console.error(error));
+  };
+  const cerrarSesion = () => {
+    localStorage.clear();
+    setSesion(null);
+  };
 
-    if (sesion === null) {
-        body.style.backgroundImage = `url(${img_2})`;
-        body.style.backgroundRepeat = "no-repeat";
-        body.style.backgroundSize = "cover";
-    } else {
-        body.style.background = "none";
-    }
-    return (
-        <BrowserRouter>
-            <Routes>
-                {(sesion === null) ? <Route path="*" element={<Login iniciarSesion={iniciarSesion}/>}/> :
-                    <Route element={<BarraDeNavegacion cerrarSesion={cerrarSesion}/>}>
-                        <Route path="/" element={<Navigate to="/principal"/>}/>
-                        <Route path="/principal" element={<Principal/>}/>
-                    </Route>
-                }
-            </Routes>
-        </BrowserRouter>
-    );
+  if (sesion === null) {
+    body.style.backgroundImage = `url(${img_2})`;
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+  } else {
+    body.style.background = "none";
+  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        {sesion === null ? (
+          <Route path="*" element={<Login iniciarSesion={iniciarSesion} />} />
+        ) : (
+          <>
+            <Route element={<BarraDeNavegacion cerrarSesion={cerrarSesion} />}>
+              <Route path="/" element={<Navigate to="/principal" />} />
+              <Route path="/principal" element={<Principal />} />
+            </Route>
+            <Route path="*" element={<E404 />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
+  );
 }
